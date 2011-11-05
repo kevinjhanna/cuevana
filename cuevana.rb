55e7b038de83d361ced114b03ccde15f49799887
Kevin J. Hanna <kevinjackhanna@gmail.com>
2011-11-05 21:23:15 -0200

    Erased returns

--- cuevana.rb
+++ cuevana.rb
@@ -44,29 +44,29 @@ class Cuevana
   def extract_last_page
     movies_url = @@URL + @@MOVIES_LIST +"0"
     doc = Nokogiri::HTML(open(movies_url).read.force_encoding("utf-8"))
-    return doc.at_xpath("/html/body/div/div[9]/span/a[6]").content.to_i
+    doc.at_xpath("/html/body/div/div[9]/span/a[6]").content.to_i
   end
   
   def extract_title(movie)
-    return movie.at_css('.tit a').content
+    movie.at_css('.tit a').content
   end
   
   def extract_id(movie)
-    return movie.at_css('.tit a @href').content.split('/')[2]
+    movie.at_css('.tit a @href').content.split('/')[2]
   end
   
   def extract_sources(movie)
     player_params = "&id=#{extract_id(movie)}&subs=,ES&onstart=yes&sub_pre=ES"
     player_link = @@URL + @@SOURCE + player_params
     player = Nokogiri::HTML(open(player_link).read.force_encoding("utf-8"))
-    return player.css("html body div div div#sources ul script")
+    player.css("html body div div div#sources ul script")
   end
   
   # returns an array of html tags that represents a movie
   def extract_movies(pageNo)
     movies_url = @@URL + @@MOVIES_LIST + pageNo.to_s
     doc = Nokogiri::HTML(open(movies_url).read.force_encoding("utf-8"))
-    return doc.xpath("//table//tr[@class != 'tabletit']")
+    doc.xpath("//table//tr[@class != 'tabletit']")
   end
   
   def extract_download_link(key, host)
@@ -74,7 +74,7 @@ class Cuevana
     uri = URI(@@URL + @@SOURCE_GET)
     res = Net::HTTP.post_form(uri, 'key' => key, 'host' => host)
     # from ascii to utf-8 compatibility
-    return res.body.gsub(/[^a-zA-Z0-9\/\.\&\?=:]/, "") 
+    res.body.gsub(/[^a-zA-Z0-9\/\.\&\?=:]/, "") 
   end
   
 end
